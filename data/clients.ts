22683e494193c35c909fccdc37ec58f57329c41c
ahmad007125 <ahmadansari007125@gmail.com>
2026-06-17 11:34:50 +0300

    Clients images updated

--- data/clients.ts
+++ data/clients.ts
@@ -2,46 +2,46 @@ export const clients = [
   {
     name: "Saudi Aramco",
     initials: "SA",
-    image: "/images/clients/1.jpeg",
+    image: "/images/clients/1.webp",
   },
   {
     name: "Al Qaryan Group",
     initials: "SB",
-    image: "/images/clients/2.jpeg",
+    image: "/images/clients/2.webp",
   },
   {
     name: "Abaja Contracting",
     initials: "NE",
-    image: "/images/clients/3.jpeg",
+    image: "/images/clients/3.webp",
   },
   {
     name: "Ahel",
     initials: "MA",
-    image: "/images/clients/4.jpeg",
+    image: "/images/clients/4.webp",
   },
   {
     name: "Al Manaratain",
     initials: "RS",
-    image: "/images/clients/5.jpeg",
+    image: "/images/clients/5.webp",
   },
   {
     name: "Saudi Cranes",
     initials: "AP",
-    image: "/images/clients/6.jpeg",
+    image: "/images/clients/6.webp",
   },
   {
     name: "KMT",
     initials: "AR",
-    image: "/images/clients/7.jpg",
+    image: "/images/clients/7.webp",
   },
   {
     name: "Zamil Industrial",
     initials: "ZI",
-    image: "/images/clients/8.jpeg",
+    image: "/images/clients/8.webp",
   },
   {
     name: "Red Sea Arabia",
     initials: "CP",
-    image: "/images/clients/9.jpg",
+    image: "/images/clients/9.webp",
   },
 ];
