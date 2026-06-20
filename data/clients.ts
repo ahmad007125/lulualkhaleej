7d3a3f0654cdb11b74ef05c1b2643bb70cd75f51
ahmad007125 <ahmadansari007125@gmail.com>
2026-06-20 09:48:12 +0300

    Clients images updated

--- data/clients.ts
+++ data/clients.ts
@@ -2,46 +2,96 @@ export const clients = [
   {
     name: "Saudi Aramco",
     initials: "SA",
-    image: "/images/clients/1.webp",
+    image: "/images/clients/client1.webp",
   },
   {
-    name: "Al Qaryan Group",
-    initials: "SB",
-    image: "/images/clients/2.webp",
+    name: "Alfanar",
+    initials: "Al",
+    image: "/images/clients/client2.webp",
   },
   {
-    name: "Abaja Contracting",
-    initials: "NE",
-    image: "/images/clients/3.webp",
+    name: "Dayim",
+    initials: "DY",
+    image: "/images/clients/client3.webp",
   },
   {
-    name: "Ahel",
-    initials: "MA",
-    image: "/images/clients/4.webp",
+    name: "Sisco",
+    initials: "Sc",
+    image: "/images/clients/client4.webp",
   },
   {
-    name: "Al Manaratain",
+    name: "Saudi Diesel",
+    initials: "SD",
+    image: "/images/clients/client5.webp",
+  },
+  {
+    name: "Enerco",
+    initials: "EN",
+    image: "/images/clients/client6.webp",
+  },
+  {
+    name: "Nesma",
+    initials: "NS",
+    image: "/images/clients/client7.webp",
+  },
+  {
+    name: "Wescosa",
+    initials: "WS",
+    image: "/images/clients/client8.webp",
+  },
+  {
+    name: "Reda",
+    initials: "RD",
+    image: "/images/clients/client9.webp",
+  },
+  {
+    name: "Abdulla Fouad",
+    initials: "AF",
+    image: "/images/clients/client10.webp",
+  },
+  {
+    name: "Al Essa",
+    initials: "AE",
+    image: "/images/clients/client11.webp",
+  },
+  {
+    name: "Al Tamimi",
+    initials: "AT",
+    image: "/images/clients/client12.webp",
+  },
+  {
+    name: "Makhawi",
+    initials: "MK",
+    image: "/images/clients/client13.webp",
+  },
+  {
+    name: "Yapi Merkezi",
+    initials: "YM",
+    image: "/images/clients/client14.webp",
+  },
+  {
+    name: "Olayan Energy",
+    initials: "OE",
+    image: "/images/clients/client15.webp",
+  },
+  {
+    name: "Saudi Electric",
+    initials: "SE",
+    image: "/images/clients/client16.webp",
+  },
+  {
+    name: "Saudi Power Transformers",
+    initials: "SP",
+    image: "/images/clients/client17.webp",
+  },
+  {
+    name: "Target",
+    initials: "TR",
+    image: "/images/clients/client18.webp",
+  },
+  {
+    name: "Rss",
     initials: "RS",
-    image: "/images/clients/5.webp",
-  },
-  {
-    name: "Saudi Cranes",
-    initials: "AP",
-    image: "/images/clients/6.webp",
-  },
-  {
-    name: "KMT",
-    initials: "AR",
-    image: "/images/clients/7.webp",
-  },
-  {
-    name: "Zamil Industrial",
-    initials: "ZI",
-    image: "/images/clients/8.webp",
-  },
-  {
-    name: "Red Sea Arabia",
-    initials: "CP",
-    image: "/images/clients/9.webp",
+    image: "/images/clients/client19.webp",
   },
 ];
